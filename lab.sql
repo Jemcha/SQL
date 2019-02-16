823459881c9bda525d1d2b7e756ac4f22979adab
Jemcha <jemchyjinka_u@mail.ru>
2019-02-16 11:45:45 +0300

    Update lab.sql

--- lab.sql
+++ lab.sql
@@ -2,6 +2,11 @@ create table Department (id integer primary key, name varchar(255));
 
 create table Employee (id integer primary key, department_id integer, cheif_doc_id integer, name varchar(255), num_public integer);
 alter table Employee add foreign key (department_id) references Department(id);
-
-https://docs.google.com/document/d/1I94e2vaalFC-r7Ke1W4sC0jh1YUP-e48xDdmVM1AECo/edit                                                              
+ 
+                                                              
+with count_cheif as (
+select department_id, count(distinct cheif_doc_id) as cnt_cheif_doc
+from Employee
+group by department_id
+order by department_id) select department.name, cnt_cheif_doc from count_cheif join Department on Department.id=count_cheif.department_id;
                                                               
