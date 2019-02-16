6f6efa2dda110700a7eac8d067f1ea7a7e8c8ff7
Jemcha <jemchyjinka_u@mail.ru>
2019-02-16 15:01:46 +0300

    Update lab.sql

--- lab.sql
+++ lab.sql
@@ -1,3 +1,5 @@
+SELECT 'ФИО: Ковалева Ирина';
+
 create table Department (id integer primary key, name varchar(255));
 
 create table Employee (id integer primary key, department_id integer, cheif_doc_id integer, name varchar(255), num_public integer);
