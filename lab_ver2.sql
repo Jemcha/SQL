fde18d7372dcd1cf3a27a72c47fefb4ec36ddcd4
Jemcha <jemchyjinka_u@mail.ru>
2019-02-16 15:46:45 +0300

    Update lab_ver2.sql

--- lab_ver2.sql
+++ lab_ver2.sql
@@ -9,3 +9,19 @@ order by department_id) select department.name, cnt_cheif_doc from count_cheif j
 select department.name, dep_chief.cht_chief
 from (select distinct department_id, count(cheif_doc_id) over (partition by department_id) cht_chief from Employee order by department_id) as dep_chief
 inner join department on department.id=dep_chief.department_id;
+
+---b
+--ver1
+with cnt3_in_dep as (
+     select department_id, cnt_emp from (
+select department_id, count(department_id) as cnt_emp
+from Employee
+group by department_id
+order by department_id) as cnt_in_dep
+where cnt_emp>=3)
+  select department_id, Department.name, cnt_emp from cnt3_in_dep join department on department.id=cnt3_in_dep.department_id;
+--ver2
+select department.name, department_id, cnt_emp from (
+  select distinct department_id, count(name) over (partition by department_id) cnt_emp from Employee order by department_id) dep_emp
+inner join Department on Department.id=dep_emp.department_id
+where cnt_emp>=3;
