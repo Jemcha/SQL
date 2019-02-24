9ab4eae001f1284b8895c32a5297e2322ca418e9
Jemcha <jemchyjinka_u@mail.ru>
2019-02-24 18:36:57 +0300

    Update Final_project.sql

--- Final_project.sql
+++ Final_project.sql
@@ -4,11 +4,28 @@ Select count(*) from book_ratings right join Books on books.id=Book_ratings.book
 
 2. Вывести список книг и их средний рейтинг
 
+Select name, avg(rating) from Book_ratings right join Books on Books.id=Book_ratings.book_id group by books.name;
 
 3. Вывести название жанров, количество книг и средний рейтинг каждого жанра
+
+
+
 4. Вывести список логинов 3 пользователей, у который средний рейтинг выше 3.5
+
+select login, avg(rating) from Readers inner join Book_ratings on readers.id=Book_ratings.reader_id  
+group by reader_id, login having avg(rating)>3.5 limit 3;
+
 5. Вывести список названий книг с макимальной оценкой
-6. Найти самую популярную книгу - вывести назнвание книги с максимальным количеством оценок и среднюю оценку
+
+with avg_rate as ( select book_id, avg(rating) as avg from Book_ratings group by book_id ) 
+select books.name, avg from avg_rate inner join Books on Books.id=avg_rate.book_id 
+where avg in( select max(avg) from avg_rate limit 1);
+
+
+6. Dывести назнвание книги с максимальным количеством оценок и среднюю оценку
+
+
+
 7. Вывести названия книг, у которых минимум 3 оценки и средний рейтинг выше 3.5
 8. Вывести список авторов, количество их книг и средний рейтинг книг этого автора
 9. Вывести список авторов, у которых есть книги хотя бы в 2 жанрах и количество их книг
